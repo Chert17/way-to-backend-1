b5e5239c0aabcad730ee2e4a1353b36546ca6fcf
Andrii Chertkov <andriichertkov@gmail.com>
2023-04-27 12:27:45 +0300

    add videos tests

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,6 @@
-import express, { Request, Response } from 'express';
+import { app } from './setting';
 
-import { videoRouter } from './routes/video.routes';
-import { STATUS_CODE } from './utils/status.code';
-import { videosData } from './utils/videos.data';
-
-export const app = express();
 const port = 3000;
-
-app.use(express.json());
-
-app.delete('/testing/all-data', (req: Request, res: Response) => {
-  videosData.splice(0);
-  res.sendStatus(STATUS_CODE.NOT_CONTENT);
-});
-
-app.use('/videos', videoRouter);
-
-app.use((req: Request, res: Response) => {
-  res.status(STATUS_CODE.NOT_FOUND).json({ message: 'Not found' });
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
